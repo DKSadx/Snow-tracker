def6251604bea04e85f450fb16b37940404ef524
DKSadx <sadx.carmon@gmail.com>
2019-09-25 21:33:15 +0000

    About modal added

--- screens/Modals/About.js
+++ screens/Modals/About.js
@@ -1,28 +1,102 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Animated, Text, View, StyleSheet, BackHandler, TouchableOpacity } from 'react-native';
+import { colors } from '../../utils/colors';
 
-export default function Help(props) {
+export default function About(props) {
+  const [fadeAnim] = useState(new Animated.Value(0)); // Sets initial value for opacity to 0
+  const [backHandlerEvent] = useState({
+    cb: () => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 300,
+      }).start(() => props.close());
+      // Only if the function returns true the action will be overwritten
+      return true;
+    },
+  });
+
+  useEffect(() => {
+    // Fade in animation when component mounts
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+    }).start();
+    // Overrides androids hardware back button so that it executes a custom action instead the native one
+    // Adds event listener on mount and removes on unmount
+    BackHandler.addEventListener('hardwareBackPress', backHandlerEvent.cb);
+    return () => BackHandler.removeEventListener('hardwareBackPress', backHandlerEvent.cb);
+  }, []);
   return (
-    <View
-      style={{
-        height: '100%',
-        width: '100%',
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        position: 'absolute',
-      }}
-    >
-      <Text style={{ marginTop: '20%', color: 'white' }} onPress={() => props.close()}>
-        About
-      </Text>
-    </View>
+    <Animated.View style={{ ...styles.overlay, opacity: fadeAnim }}>
+      <View style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.title}>About</Text>
+        </View>
+        <View style={styles.about}>
+          <Text style={styles.aboutText}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est iste cum velit laboriosam
+            alias quibusdam soluta nam nemo doloremque, consequatur nisi. Fuga, perferendis
+            dignissimos? Sunt veniam alias rerum est aut.
+          </Text>
+        </View>
+        <TouchableOpacity onPress={() => backHandlerEvent.cb()} style={styles.closeBtn}>
+          <Text style={styles.closeBtnText}>close</Text>
+        </TouchableOpacity>
+      </View>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // style
+  overlay: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    position: 'absolute',
+    justifyContent: 'center',
   },
-  text: {
-    // style
+  container: {
+    height: '35%',
+    width: '140%',
+    backgroundColor: colors.infoModal,
+    position: 'relative',
+    left: '13.33%',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  titleView: {
+    flex: 0.8,
+    alignSelf: 'center',
+    borderBottomColor: '#fff',
+    borderBottomWidth: 2,
+    marginTop: '3%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+  },
+  title: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  about: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: '5%',
+  },
+  aboutText: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+  closeBtn: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: '3%',
+  },
+  closeBtnText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
