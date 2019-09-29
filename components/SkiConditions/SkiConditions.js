673e0b7b312da1f5a26bdf77b891ed7de74011f5
DKSadx <sadx.carmon@gmail.com>
2019-09-29 14:35:07 +0000

    SkiConditions logic added

--- components/SkiConditions/SkiConditions.js
+++ components/SkiConditions/SkiConditions.js
@@ -4,10 +4,36 @@ import { Text, View, StyleSheet } from 'react-native';
 import { textStyles } from '../../utils/textStyles';
 
 export default function SkiConditions(props) {
+  const { currently } = props.data;
+  const { temperature, icon, precipIntensity } = currently;
+  const windSpeed = Math.round((currently.windSpeed / 1000) * 360000) / 100;
+  let score = 0;
+  let skiConditions;
+  let color;
+  // Calculates score depending on the current weather conditions
+  if (temperature > 5 || windSpeed > 8 || (icon === 'rain' && precipIntensity > 5)) {
+    score = 0;
+  } else {
+    if (temperature < 0 && temperature > -12) score = score + 2;
+    else score = score + 1;
+    if (windSpeed < 4) score = score + 2;
+    else score = score + 1;
+  }
+  if (score > 2) {
+    skiConditions = 'Good';
+    color = '#29cf04';
+  } else if (score > 0 && score <= 2) {
+    skiConditions = 'Moderate';
+    color = 'yellow';
+  } else {
+    skiConditions = 'Bad';
+    color = 'red';
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Ski conditions: <Text style={textStyles.bold}>Bad</Text>
+      <Text style={{ ...styles.text, color }}>
+        Ski conditions: <Text style={textStyles.bold}>{skiConditions}</Text>
       </Text>
     </View>
   );
@@ -19,7 +45,6 @@ const styles = StyleSheet.create({
     marginBottom: '5%',
   },
   text: {
-    color: 'red',
     textAlign: 'center',
   },
 });
